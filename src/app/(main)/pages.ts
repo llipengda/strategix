0d1d7f67cfb09731124ee841c522f8f9356a8730
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2024-11-06 15:48:39 +0800

    feat: 创建schedule路由,描绘日历推送的布局

--- src/app/(main)/pages.ts
+++ src/app/(main)/pages.ts
@@ -1,6 +1,7 @@
 const pages = [
   { name: '团队', url: '/team' },
-  { name: '用户', url: '/user' }
+  { name: '用户', url: '/user' },
+  {name:'计划', url: '/schedule'}
 ] satisfies {
   name: string
   url: string
