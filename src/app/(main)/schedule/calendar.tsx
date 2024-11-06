ee78cd7a117538ce9e8924c1904ab126b4408316
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2024-11-06 23:02:24 +0800

    feat: 完成了日历的格式显示效果和数据拉取刷新等

--- src/app/(main)/schedule/calendar.tsx
+++ src/app/(main)/schedule/calendar.tsx
@@ -1,25 +1,96 @@
+import Input from "@/components/input";
+import SubmitButton from "@/components/submit-button";
+import { getPosts } from "@/lib/actions/post";
+import { Post } from "@/types/post";
 
-const generateDateInfo = () => {
-  for(let i=0;i<35;i++){
-
+const getDayName = (day: number) => {
+  switch (day) {
+    case 0: return "日";
+    case 1: return "一";
+    case 2: return "二";
+    case 3: return "三";
+    case 4: return "四";
+    case 5: return "五";
+    case 6: return "六";
+    default: return "未知";
   }
 }
-const Calendar = ({
-  onItemClick
-}:{
-  onItemClick: (year: number, month: number)=> void
-}) => {
-  return (
-    <div className=' w-full grid grid-cols-7 grid-rows-5 flex-grow gap-1'>
-      {'1'.repeat(31).split('').map((v, index) => {
+interface IDateInfo {
+  day: number
+  date?: number
+  posts?: Post[]
+}
+const generateDateInfo = async (year: number, month: number) => {
+  const postInfo: Post[] = await getPosts(year, month)
+  const dateInfo: IDateInfo[] = []
+  const getHowManyDate = (year: number, month: number) => {
+    switch (month) {
+      case 1: return 31;
+      case 2:
+        // 判断是否是闰年
+        if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+          return 29;
+        }
+        return 28;
+      case 3: return 31;
+      case 4: return 30;
+      case 5: return 31;
+      case 6: return 30;
+      case 7: return 31;
+      case 8: return 31;
+      case 9: return 30;
+      case 10: return 31;
+      case 11: return 30;
+      case 12: return 31;
+      default: return 30;
+    }
+  }
+  const dateNum = getHowManyDate(year, month);
+  const predays = new Date(year, month - 1, 1).getDay();
 
-        return(
-          <div className='bg-amber-300 rounded-sm' key={index}>
-            {index}
-          </div>
-        )
-      })}
-    </div>
+  for (let i = 0; i < predays; i++)
+    dateInfo.push({ day: i });
+  for (let i = 1; i <= dateNum; i++) {
+    dateInfo.push({
+      day: (predays + i) % 7, 
+      date: i,
+      posts: postInfo.filter(v => {
+        const d = new Date(v.publishDate)
+        return d.getDate() === i
+      })
+    });
+  }
+  return dateInfo
+}
+
+
+
+const Calendar = async () => {
+  const dateInfo = await generateDateInfo(new Date().getFullYear(), new Date().getMonth() + 1)
+
+  return (
+    <>
+      <Input type="month" placeholder="选择月份" />
+      <SubmitButton text="查询" />
+      <div className=' w-full grid grid-cols-7 grid-rows-5 flex-grow gap-1'>
+        {dateInfo.map((v, index) => {
+
+          return (
+            <div className={`${v.date ? 'bg-amber-300' : 'bg-slate-500'} rounded-sm`} key={index}>
+              <div>{getDayName(v.day)}</div>
+              <div>{v.date}</div>
+              <div>{v.posts?.map((v, index) => {
+                return (
+                  <div key={index} className='text-xs'>{v.title}</div>
+                )
+              })
+              }</div>
+            </div>
+          )
+        })}
+      </div>
+    </>
+
   )
 }
 
--- src/app/(main)/schedule/page.tsx
+++ src/app/(main)/schedule/page.tsx
@@ -1,41 +1,15 @@
-import PostItem from "@/app/(main)/schedule/post-item";
 import Calendar from "@/app/(main)/schedule/calendar";
-import {createPostAction, getPosts} from "@/lib/actions/post";
-import { Post } from "@/types/post";
 import AddPosts from "./add-post";
-
 const Page = async () => {
-
-  const year = new Date().getFullYear()
-  const month = new Date().getMonth() + 1
-  const postInfo: Post[] = await getPosts(year, month)
   return (
-
       <div className='w-full h-full flex flex-col *:w-full gap-2'>
-
-          <div className='bg-pink-500 flex flex-col aspect-video w-3/4 mx-auto'>
-            <div className=' h-6 text-xl bg-blue-300'>
-              日历
-            </div>
-            <Calendar onItemClick={()=>{}} />
-
+          <div className='flex flex-col aspect-video w-3/4 mx-auto'>
+            <Calendar />
           </div>
-
         <div className='flex flex-col gap-0.5 *:rounded-md'>
-
               <AddPosts />
-
-
-            {postInfo.map(
-              v => (
-                <PostItem title={v.title} teamName={v.team} key={v.id} />
-              )
-            )}
-
         </div>
       </div>
-
   )
 }
-
 export default Page
