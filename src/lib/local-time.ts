b152ce3bf9c7b5dc36c6d28c6402fc32fbce4ebe
llipengda <lipengda2@outlook.com>
2024-11-07 09:56:34 +0800

    fix time bug

--- src/lib/local-time.ts
+++ src/lib/local-time.ts
@@ -1,4 +1,5 @@
-// UTC+8. This is because vercel's server uses UTC time.
-const localTime = () => new Date(new Date().toUTCString() + 8 * 60 * 60 * 1000)
+process.env.TZ = 'Asia/Shanghai'
+
+const localTime = () => new Date()
 
 export default localTime
