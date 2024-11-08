a22d4909ef12b5b58eaedaf81c9a4576c61f784b
llipengda <lipengda2@outlook.com>
2024-11-08 22:55:32 +0800

    feat: 优化日历组件的年份和月份参数处理

--- src/app/(main)/schedule/page.tsx
+++ src/app/(main)/schedule/page.tsx
@@ -1,17 +1,16 @@
 import Calendar from '@/app/(main)/schedule/calendar'
+import { localDate } from '@/lib/time'
 
 import AddPosts from './add-post'
 
 const Page = async (props: PageProps) => {
   const p = await props.searchParams
-  const year =
-    p?.year === undefined ? new Date().getFullYear() : parseInt(p.year)
-  const month =
-    p?.month === undefined ? new Date().getMonth() + 1 : parseInt(p.month)
+  const year = p?.year ?? localDate().getFullYear()
+  const month = p?.month ?? localDate().getMonth() + 1
   return (
     <div className='w-full h-full flex flex-col *:w-full gap-2'>
       <div className='flex flex-col aspect-video w-3/4 mx-auto dark:border-white/10 border-2 rounded-md p-2'>
-        <Calendar year={year} month={month} />
+        <Calendar year={Number(year)} month={Number(month)} />
       </div>
       <div className='flex flex-col gap-0.5 *:rounded-md'>
         <AddPosts />
