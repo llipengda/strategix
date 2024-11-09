30a97ce35b33c63c9c78a7cbdbcfd0289ea70e73
llipengda <lipengda2@outlook.com>
2024-11-10 01:10:30 +0800

    feat: 为各个回退组件添加唯一标识符

--- src/app/(main)/fallbacks.tsx
+++ src/app/(main)/fallbacks.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import { useId } from 'react'
 import ContentLoader from 'react-content-loader'
 
 import useTheme from '@/lib/hooks/use-theme'
 
 export const WeatherFallback = () => {
   const theme = useTheme()
+  const id = useId()
 
   const isDark = theme === 'dark'
 
@@ -14,7 +16,7 @@ export const WeatherFallback = () => {
       className='min-h-[280px] min-w-[360px] max-xl:min-w-[512px] max-lg:min-w-min max-lg:w-full'
       backgroundColor={isDark ? '#111' : '#eee'}
       foregroundColor={isDark ? '#333' : '#f5f5f5'}
-      uniqueKey='weather-fallback'
+      uniqueKey={id}
     >
       <rect x='0' y='0' rx='4' ry='4' width='100%' height='100%' />
     </ContentLoader>
@@ -23,6 +25,7 @@ export const WeatherFallback = () => {
 
 export const ScheduleFallback = () => {
   const theme = useTheme()
+  const id = useId()
 
   const isDark = theme === 'dark'
 
@@ -31,7 +34,7 @@ export const ScheduleFallback = () => {
       className='h-80 min-w-[360px] max-xl:min-w-[512px] max-lg:min-w-min max-lg:w-full'
       backgroundColor={isDark ? '#111' : '#eee'}
       foregroundColor={isDark ? '#333' : '#f5f5f5'}
-      uniqueKey='schedule-fallback'
+      uniqueKey={id}
     >
       <rect x='0' y='0' rx='4' ry='4' width='100%' height='100%' />
     </ContentLoader>
@@ -40,6 +43,7 @@ export const ScheduleFallback = () => {
 
 export const AnnouncementFallback = () => {
   const theme = useTheme()
+  const id = useId()
 
   const isDark = theme === 'dark'
 
@@ -48,7 +52,7 @@ export const AnnouncementFallback = () => {
       className='min-w-[512px] max-lg:min-w-min max-lg:w-full min-h-80'
       backgroundColor={isDark ? '#111' : '#eee'}
       foregroundColor={isDark ? '#333' : '#f5f5f5'}
-      uniqueKey='announcement-fallback'
+      uniqueKey={id}
     >
       <rect x='0' y='0' rx='4' ry='4' width='100%' height='100%' />
     </ContentLoader>
@@ -57,6 +61,7 @@ export const AnnouncementFallback = () => {
 
 export const ClockFallback = () => {
   const theme = useTheme()
+  const id = useId()
 
   const isDark = theme === 'dark'
 
@@ -67,7 +72,7 @@ export const ClockFallback = () => {
       height={200}
       backgroundColor={isDark ? '#111' : '#eee'}
       foregroundColor={isDark ? '#333' : '#f5f5f5'}
-      uniqueKey='clock-fallback'
+      uniqueKey={id}
     >
       <circle r={80} cx={100} cy={100} />
     </ContentLoader>
