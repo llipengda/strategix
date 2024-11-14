8a8f65dba587e336c929a03648c3feb30d9e4fec
邓涵杰 <87105391+floatDreamWithSong@users.noreply.github.com>
2024-11-14 21:42:02 +0800

    fix: 非Admin用户现在不会看到公告板的删除功能和发布功能了

--- src/app/(main)/announcement/page.tsx
+++ src/app/(main)/announcement/page.tsx
@@ -7,25 +7,36 @@ import { AddAnnouncement } from './add-announcement'
 const AnnouncementPage = async () => {
   const announcements = await getAllAnnouncements()
   const user = await getCurrentUser()
+  const baseRight = user?.role === 'admin' || user?.role === 'super-admin'
+  const queryDeleteRight = (publisherId?: string) => {
+    if (user?.role === 'super-admin')
+      return true
+    if (user?.role === 'admin')
+      return user.id === publisherId
+    return false
+  }
+
   return (
     <div className='flex max-lg:flex-col gap-8'>
       {announcements.length === 0 ? (
-        <div className='w-full min-h-56 flex items-center justify-center'>
+        <div className={`min-h-56 flex items-center justify-center ${baseRight ? 'w-3/5' : 'w-full'}`}>
           <p className='text-center my-auto text-gray-400 italic'>
             暂时没有公告
           </p>
         </div>
       ) : (
-        <div className='p-8 bg-white dark:bg-gray-900 rounded-md shadow-md w-3/5 max-lg:w-full'>
+        <div className={`p-8 bg-white dark:bg-gray-900 rounded-md shadow-md max-lg:w-full ${baseRight ? 'w-3/5' : 'w-full'}`} >
           <h1 className='text-3xl font-bold mb-6'>公告</h1>
           <div className='flex flex-col gap-4'>
             {announcements.map(a => (
-              <Announcement key={a.id} announcement={a} draggable={false} />
+              <Announcement key={a.id} announcement={a} draggable={false} showDelete={queryDeleteRight(a.publisherId)} />
             ))}
           </div>
         </div>
       )}
-      <AddAnnouncement user={user} />
+      {
+        baseRight && (<AddAnnouncement user={user} />)
+      }
     </div>
   )
 }
