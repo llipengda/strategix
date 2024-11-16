be174022275e9146f9a6b5cf39f6858b1a8d8504
llipengda <llipengda@users.noreply.github.com>
2024-11-16 16:41:12 +0000

    [format] Prettierfy code

--- src/lib/b2.ts
+++ src/lib/b2.ts
@@ -1,12 +1,13 @@
 'use server'
 
-import { auth } from '@/auth'
 import {
   GetObjectCommand,
   PutObjectCommand,
   S3Client
 } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+
+import { auth } from '@/auth'
 
 const b2Client = new S3Client({
   region: process.env.B2_REGION,
