bd1faed4d57ef7950e53748642d45d1f01bfb0e5
llipengda <lipengda2@outlook.com>
2024-11-20 23:17:35 +0800

    fix: remove head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,30 +57,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='zh-Hans'>
-      <head>
-        <meta name='application-name' content='STRATEGIX' />
-        <meta name='apple-mobile-web-app-capable' content='yes' />
-        <meta name='apple-mobile-web-app-status-bar-style' content='default' />
-        <meta name='apple-mobile-web-app-title' content='STRATEGIX' />
-        <meta name='description' content='STRATEGIX' />
-        <meta name='format-detection' content='telephone=no' />
-        <meta name='mobile-web-app-capable' content='yes' />
-        <meta name='theme-color' content='#000000' />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='192x192'
-          href='/icon-192x192.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='512x512'
-          href='/icon-512x512.png'
-        />
-        <link rel='manifest' href='/manifest.json' />
-        <link rel='shortcut icon' href='/favicon.ico' />
-      </head>
+      <head />
       <body className='antialiased bg-page-bg relative'>
         {!isProduction && <BreakpointDisplay />}
         <NextTopLoader />
