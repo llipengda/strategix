12b2468ff6c1dfb5b3ef36a5f15236bb4f9c7363
llipengda <lipengda2@outlook.com>
2024-11-21 00:37:28 +0800

    fix: fix config

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,8 @@ import withSerwistInit from '@serwist/next'
 const withSerwist = withSerwistInit({
   // Note: This is only an example. If you use Pages Router,
   // use something else that works, such as "service-worker/index.ts".
-  swSrc: '/src/app/sw.ts',
-  swDest: '/public/sw.js'
+  swSrc: 'src/app/sw.ts',
+  swDest: 'public/sw.js'
 })
 
 export default withSerwist({
