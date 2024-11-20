b0487dc20ffbbf54181efdfa46ff65eb99f63480
llipengda <lipengda2@outlook.com>
2024-11-20 22:45:10 +0800

    fix: pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,17 @@
-import nextPWA from 'next-pwa'
+import withSerwistInit from '@serwist/next'
 
-const withPWA = nextPWA({
-  dest: 'public'
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js'
 })
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+export default withSerwist({
   experimental: {
     reactCompiler: true,
     serverActions: {
       bodySizeLimit: '5mb'
     }
   }
-}
-
-export default withPWA(nextConfig)
+})
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,51 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
 import NextTopLoader from 'nextjs-toploader'
 
 import '@/app/globals.css'
 import BreakpointDisplay from '@/components/breakpoint-display'
 
+const APP_NAME = 'STRATEGIX'
+const APP_DEFAULT_TITLE = 'STRATEGIX'
+const APP_TITLE_TEMPLATE = '%s - STRATEGIX'
+const APP_DESCRIPTION = 'STRATEGIX is an AI-powered strategy management tool'
+
 export const metadata: Metadata = {
-  title: 'STRATEGIX',
-  description: 'STRATEGIX is an AI-powered strategy management tool'
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE
+    // startupImage: []
+  },
+  formatDetection: {
+    telephone: false
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  }
+}
+
+export const viewport: Viewport = {
+  themeColor: '#FFFFFF'
 }
 
 const isProduction = process.env.NODE_ENV === 'production'
